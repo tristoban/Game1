8ec24c2cce8dd0af315dabdd5f3e08009f8165ff
Matias <hola@cloomy.us>
2022-08-24 17:01:41 -0300

    Volví a método 1 de Run

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -38,7 +38,7 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread = new Thread(this); //instancio el "hilo" señalando this como la clase general GamePanel
         gameThread.start();//comienza
     }
-/*
+
     @Override
     public void run() {
         while(gameThread != null){
@@ -66,15 +66,15 @@ public class GamePanel extends JPanel implements Runnable {
                 throw new RuntimeException(e);
             }
         }
-    }*/
-
+    }
+/*
     //Otro método de runear
     public void run(){
         double drawInterval = 1000000000/FPS;
         double delta = 0;
         long last
     }
-
+*/
 
 
     public void update(){
